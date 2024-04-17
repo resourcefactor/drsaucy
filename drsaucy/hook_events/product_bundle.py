d5801d2f2a4ff3227b4e28f6fff777d731442cd7
hamzaali15 <malikhamzaali48@gmail.com>
2024-04-17 23:57:33 +0500

    feat: product bundle sub items table added and fetched

---
+++ drsaucy/hook_events/product_bundle.py
@@ -0,0 +1,94 @@
+# Copyright (c) 2022, Frappe Technologies Pvt. Ltd. and Contributors
+# License: GNU General Public License v3. See license.txt
+
+
+from operator import itemgetter
+import frappe
+from frappe import _
+from frappe.utils import flt
+from erpnext.selling.doctype.product_bundle.product_bundle import ProductBundle
+
+
+class OverrideProductBundle(ProductBundle):
+	def validate_child_items(self):
+		pass
+
+
+def add_sub_items_on_validate(self, method):
+	update_sub_items(self, save=False)
+
+
+def update_sub_items(self, save=True):
+	get_sub_items(self)
+	add_sub_items(self, save=save)
+
+
+def get_sub_items(self):
+	self.cur_sub_items = {}
+	for d in self.items:
+		if frappe.db.exists("Product Bundle", {"new_item_code": d.item_code}):
+			get_child_sub_items(self, d.item_code, d.qty)
+		elif d.item_code:
+			add_to_cur_sub_items(
+				self,
+				frappe._dict(
+					{
+						"item_code": d.item_code,
+						"description": d.description,
+						"uom": d.uom,
+						"qty": flt(d.qty),
+					}
+				)
+			)
+
+
+def add_to_cur_sub_items(self, args):
+	if self.cur_sub_items.get(args.item_code):
+		self.cur_sub_items[args.item_code]["qty"] += args.qty
+	else:
+		self.cur_sub_items[args.item_code] = args
+
+
+def get_child_sub_items(self, item_code, qty):
+	child_fb_items = frappe.db.sql(
+		"""
+		SELECT
+			pbs_item.item_code,
+			pbs_item.description,
+			pbs_item.uom,
+			pbs_item.qty
+		FROM `tabProduct Bundle` as pb
+		JOIN `tabProduct Bundle Item` as pbs_item on pb.name = pbs_item.parent
+		WHERE
+			pb.new_item_code = '{0}'
+	""".format(item_code), as_dict=1)
+
+	for d in child_fb_items:
+		add_to_cur_sub_items(
+			self,
+			frappe._dict(
+				{
+					"item_code": d["item_code"],
+					"description": d["description"],
+					"uom": d["uom"],
+					"qty": d["qty"],
+				}
+			)
+		)
+
+
+def add_sub_items(self, save=True):
+	"Add items to Flat BOM table"
+	self.set("custom_subitems", [])
+
+	if save:
+		frappe.db.sql("""delete from `tabProduct Bundle Sub Item` where parent=%s""", self.name)
+
+	for d in sorted(self.cur_sub_items, key=itemgetter(0)):
+		ch = self.append("custom_subitems", {})
+		for i in self.cur_sub_items[d].keys():
+			ch.set(i, self.cur_sub_items[d][i])
+		ch.docstatus = self.docstatus
+
+		if save:
+			ch.db_insert()
--- drsaucy/hooks.py
+++ drsaucy/hooks.py
@@ -114,9 +114,9 @@ app_license = "mit"
 # ---------------
 # Override standard doctype classes
 
-# override_doctype_class = {
-# 	"ToDo": "custom_app.overrides.CustomToDo"
-# }
+override_doctype_class = {
+	"Product Bundle": "drsaucy.hook_events.product_bundle.OverrideProductBundle",
+}
 
 # Document Events
 # ---------------
@@ -129,6 +129,12 @@ app_license = "mit"
 # 		"on_trash": "method"
 # 	}
 # }
+
+doc_events = {
+	"Product Bundle": {
+		"validate": "drsaucy.hook_events.product_bundle.add_sub_items_on_validate"
+	}
+}
 
 # Scheduled Tasks
 # ---------------
